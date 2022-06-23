2fe00ef267edbb777225781e2d96634839449fcf
joanascheer <scheer.joana@gmail.com>
2022-06-23 12:28:35 -0300

    alterando register

---
+++ MovieFlix/app/src/main/java/br/com/zup/movieflix/register/model/RegisterModel.kt
@@ -0,0 +1,7 @@
+package br.com.zup.movieflix.register.model
+
+class RegisterModel(
+    var username: String,
+    var email: String,
+    var password: String
+)
---
+++ MovieFlix/app/src/main/java/br/com/zup/movieflix/register/repository/RegisterRepository.kt
@@ -0,0 +1,13 @@
+package br.com.zup.movieflix.register.repository
+
+import br.com.zup.movieflix.register.model.RegisterModel
+
+class RegisterRepository {
+    fun recoverData() {
+
+    }
+    fun registeredUsers(registeredUser: RegisterModel) {
+
+
+    }
+}
